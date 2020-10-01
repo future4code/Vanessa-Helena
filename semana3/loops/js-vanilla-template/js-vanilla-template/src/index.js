884a14f3fd53ac2deebbd87fecab0c9d56d3b0e2
Vanessasz <vanessa182vans@gmail.com>
2020-10-01 16:56:37 -0300

    Exercícios de laços

---
+++ semana3/loops/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,50 @@
+
+// Exercícios de interpretação de código 
+
+// EXERCÍCIO 1
+
+
+// EXERCÍCIO 2 
+
+// a. Nesse exercício ele vai printar todos os números da lista
+// que forem maior que 18. 
+
+// b. Daria sim criaria uma variável let index of lista e printatia meu index no console.log
+// assim mostraria todos os meus números dentro dessa lista.
+
+
+// Exercícios de escrita de código 
+
+// EXERCÍCIO 3
+
+// Resposta a. 
+
+//let arrayOriginal = [1, 2 , 3, 4, 8, 9, 10]
+//for (let numeros of arrayOriginal) {
+    //console.log(numeros)
+//}
+
+// Resposta b. 
+
+//let arrayOriginal = [1, 2 , 3, 4, 8, 9, 10]
+//for (let i = 0; i < arrayOriginal.length; i = i + 1) {
+    //console.log(arrayOriginal[i]/10)
+    //}
+
+// Resposta c. 
+
+//let arrayOriginal = [1, 2 , 3, 4, 8, 9, 10]
+
+//let numerosPares = []
+
+
+
+//for (let i = 0; i < arrayOriginal.length; i = i + 1) {
+
+    
+    //if (arrayOriginal[i] % 2 === 0) {
+        //numerosPares.push(arrayOriginal[i])
+        
+        //console.log(numerosPares[i])
+
+//}
