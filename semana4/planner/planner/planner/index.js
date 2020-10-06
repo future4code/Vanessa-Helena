4dc77d2c41c68424b6aaaef76b58d7c30c33cebe
Vanessasz <vanessa182vans@gmail.com>
2020-10-06 16:44:06 -0300

    Fazendo um planner digital

---
+++ semana4/planner/planner/planner/index.js
@@ -0,0 +1,13 @@
+
+
+
+//function pegarInput() {
+  //let inputUsuario = document.getElementById("tarefa").value;
+
+  //inputUsuario.value;
+  //let diaSemana = document.getElementById("dias-semana");
+  //const tarefa = document.getElementById(diaSemana.value);
+//}
+
+//let diaSemana = document.getElementById("dias-semana")
+//console.log(segunda)
