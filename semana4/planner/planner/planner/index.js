a8c2bcce9525919d62f3cfbe164cc90c8aadea17
Vanessasz <vanessa182vans@gmail.com>
2020-10-06 21:06:55 -0300

    Terminei a tarefa e fiz o exercício 1 do desafio

--- semana4/planner/planner/planner/index.js
+++ semana4/planner/planner/planner/index.js
@@ -1,13 +1,45 @@
+function pegarInput() {
+  let inputUsuario = document.getElementById("tarefa").value;
 
+if (inputUsuario == "") {
+    alert("Tarefa não pode ser vazia, digite uma tarefa.");
+    let inputUsuario = document.getElementById("tarefa")
+        inputUsuario.value = "" 
+}
+        let diaSemana = document.getElementById("dias-semana");
 
+  switch (diaSemana.value) {
+    case "domingo":
+      document.getElementById("domingo").innerHTML += `<p>${inputUsuario}</p>`;
+      break;
+    case "segunda":
+      document.getElementById("segunda").innerHTML +=`<p>${inputUsuario}</p>`;
+      break;
+    case "terca":
+      document.getElementById("terca").innerHTML += `<p>${inputUsuario}</p>`;
+      break;
+    case "quarta":
+      document.getElementById("quarta").innerHTML += `<p>${inputUsuario}</p>`;
+      break;
+    case "quinta":
+      document.getElementById("quinta").innerHTML += `<p>${inputUsuario}</p>`;
+      break;
+    case "sexta":
+      document.getElementById("sexta").innerHTML += `<p>${inputUsuario}</p>`;
+      break;
+    case "sabado":
+      document.getElementById("sabado").innerHTML += `<p>${inputUsuario}</p>`;
+      break;
+  }
+}
 
-//function pegarInput() {
-  //let inputUsuario = document.getElementById("tarefa").value;
+function mensagemAlertando() {
+    
 
-  //inputUsuario.value;
-  //let diaSemana = document.getElementById("dias-semana");
-  //const tarefa = document.getElementById(diaSemana.value);
-//}
+}
 
 //let diaSemana = document.getElementById("dias-semana")
 //console.log(segunda)
+//`<li>${meuInput.value}</li>`
+
+
