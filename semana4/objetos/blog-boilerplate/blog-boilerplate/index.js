42eee9861a602757df93f561614f6e0cbd801aa3
Vanessasz <vanessa182vans@gmail.com>
2020-10-07 20:42:24 -0300

    Terminei o que não consegui fazer na parte da tarde

--- semana4/objetos/blog-boilerplate/blog-boilerplate/index.js
+++ semana4/objetos/blog-boilerplate/blog-boilerplate/index.js
@@ -1,39 +1,46 @@
 function inserindoDados() {
-  
-
-    let titulo = document.getElementById ("titulo-post")
-  
-  let autor = document.getElementById ("autor-post")
-
-  let conteudo = document.getElementById ("conteudo-post")
-
-  
-    let formulario = {
-    titulo: titulo.value,
-    autor: autor.value,
-    conteudo: conteudo.value
-  };
-
-  
-  const formLista = ["titulo", "autor", "conteudo"];
-    
-
-  document.getElementById("form").innerHTML += `<p>${titulo.value}</p>`;
-  document.getElementById("form").innerHTML += `<p>${autor.value}</p>`;
-  document.getElementById("form").innerHTML += `<p>${conteudo.value}</p>`;
-
-  titulo.value = "";
-  autor.value = "";
-    conteudo.value = "";
-
-//console.log(formLista)
-
-  
-    //console.log(formLista)
-  
-
-    //et titulo = document.getElementById("titulo-post")
-  
+  //let formulando = {
+  // titulo: titulo.value,
+  //  autor: autor.value,
+  // conteudo: conteudo.value
+  //};
+  // const formLista = ["titulo", "autor", "conteudo"];
+  // formLista.push(formulando)
+  //console.log(formulando)
+  //document.getElementById("form").innerHTML += `<p>${titulo.value}</p>`;
+  // document.getElementById("form").innerHTML += `<p>${autor.value}</p>`;
+  //document.getElementById("form").innerHTML += `<p>${conteudo.value}</p>`;
+  //titulo.value = ""; // Deixar o input vazio após escrever algo
+  //autor.value = "";
+  //conteudo.value = "";
 }
 
+function lista() {
+  let titulo = document.getElementById("titulo-post");
 
+  let autor = document.getElementById("autor-post");
+
+  let conteudo = document.getElementById("conteudo-post");
+
+  let formulando = { // Formulando meu objeto
+    titulo: titulo.value, // Titulo chave 
+    autor: autor.value, // autor chave
+    conteudo: conteudo.value, // conteudo chave
+  };
+
+  const formLista = [];
+  formLista.push(formulando);
+
+  for (i = 0; i < formLista.length; i++) {
+    document.getElementById(
+      "form"
+    ).innerHTML += `<p>Título: ${titulo.value}</p>`;
+    document.getElementById("form").innerHTML += `<p>Autor: ${autor.value}</p>`;
+    document.getElementById(
+      "form"
+    ).innerHTML += `<p>Conteúdo: ${conteudo.value}</p>`;
+    titulo.value = "";
+    autor.value = "";
+    conteudo.value = "";
+  }
+}
