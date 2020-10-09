7a788e05ede5baefe03c8301d327b132ee33bfc1
Vanessasz <vanessa182vans@gmail.com>
2020-10-09 13:17:41 -0300

    Exercícios de interpretação de código 1-4

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,24 @@
+// Exercícios de interpretação de código
+
+// 1. Ele tem uma função que tá pedindo para o usuário digitar a cotação do dólar aí o programa vai ler
+//esse número e vai multiplicar com 100 para transformar em reais e aí ele faz a chamada da função põe 
+// o resutado do cálculo em uma variável e no final vai mostrar o resultado no console. 
+
+// 2. É um programa que pede um valor para o usuário do quanto ele quer investir e lá tem vários opções de
+//investimento daí ele vai pegar o valor do usuário e vai fazer uma multiplicação referente a esse investimento
+// e em baixo existem duas variáveis uma que se chama novoMontante e segundoMontante a primeira diz que 
+// o tipo de investimento é ações e o valor é 150 ou seja já passa um valor pronto e a segunda variável
+// corresponde a um tipo de valor que não existe no programa, ou seja se rodar segundo montante vai retornar 
+// tipo de investimento informado incorreto.
+
+// 3. Nesse exercício ele verifica se um número é par ou ímpar se for par ele coloca no array vazio 1 os
+// valores e se for ímpar ele coloca no segundo array e no final ele mostra o tamanho 
+// do array dos números pares e ímpar e depois separado o tamanho da lista de números pares e o tamanho de 
+// números ímpar.
+
+// 4. O infinity sempre será o maior número então se colocar o valor 25 no programa ele vai verificar se o
+// 25 é menor que o infinity?  é, então ele vai colocar o número 25 e em baixo ele faz outra verificação
+// o 25 é maior que zero? é maior então vai mostrar o número 25. 
+
+//------------------------------------------------------------------------------------------------------
+
