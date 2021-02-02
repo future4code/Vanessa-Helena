952fe1a6da673d134d3a826a9b107badb4523872
Vanessasz <vanessa182vans@gmail.com>
2021-02-02 16:36:48 -0300

    Exercicios do 1 ao 3 ok

---
+++ semana19/exercicios-aula55/template-arquitetura2/src/controller/routes/taskRouter.ts
@@ -0,0 +1,8 @@
+import { create } from "domain"
+import express from "express"
+import { createTask, getTaskById } from "../taskController"
+
+export const taskRouter = express.Router()
+
+taskRouter.put("/", createTask)
+taskRouter.get("/:id", getTaskById)
