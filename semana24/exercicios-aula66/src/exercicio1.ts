873504add093c6aaeae4d461fb86adfcf8b9e997
Vanessasz <vanessa182vans@gmail.com>
2021-03-17 16:18:54 -0300

    Exercício 1 e 2 ok

---
+++ semana24/exercicios-aula66/src/exercicio1.ts
@@ -0,0 +1,24 @@
+// a. Receba um número e imprima todos os inteiros de 0 até esse número no console em ordem crescente
+
+// let numbers = [20, 50, 24, 30, 89, 10].sort(function (number, numberIndice) {
+//   return number - numberIndice;
+// });
+// console.log("ordem crescente: " + numbers);
+
+//   const numbers = (n:number) => {
+//      if (n >= 0) {
+//          numbers(n - 1);
+//          console.log(n);
+//        }
+//      };
+//  numbers(2);
+
+// b. Receba um número e imprima todos os inteiros desse número até 0 em ordem decrescente
+
+//  const numbers = (n:number) => {
+//      if (n >= 0) {
+//          console.log(n);
+//         numbers(n - 1);
+//        }
+//      };
+//   numbers(5);
---
+++ semana24/exercicios-aula66/src/exercicio2.ts
@@ -0,0 +1,13 @@
+// // Escreva uma função recursiva que calcule a soma dos números inteiros de 0 a n
+
+// export const calculateSumTo = (n: number, acc: number = 0): number => {
+//     if (n === 0) {
+//       return acc;
+//     }
+//     return calculateSumTo(n - 1, acc + n);
+//   };
+  
+//   console.log(calculateSumTo(3));
+//   console.log(calculateSumTo(20));
+//   console.log(calculateSumTo(20));
+  
